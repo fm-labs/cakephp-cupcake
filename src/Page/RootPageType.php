f46b152dc98f0d52ae4e37da43687793694dae99
fm-labs <flowmotionlabs@gmail.com>
2016-05-20 18:40:38 +0200

    added getUrl() method

--- src/Page/RootPageType.php
+++ src/Page/RootPageType.php
@@ -1,6 +1,12 @@
 <?php
 namespace Banana\Page;
 
+use Cake\Datasource\EntityInterface;
+
 class RootPageType extends AbstractPageType
 {
+    public function getUrl(EntityInterface $page)
+    {
+        return '/';
+    }
 }
