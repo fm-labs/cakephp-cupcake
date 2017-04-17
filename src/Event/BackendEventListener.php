234dc86db5ea1eeb1a797591055f2c2908c7b278
fm-labs <flowmotionlabs@gmail.com>
2017-04-17 12:17:35 +0200

    added BackendEventListener

---
+++ src/Event/BackendEventListener.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Banana\Event;
+
+
+use Cake\Event\Event;
+use Cake\Event\EventListenerInterface;
+
+class BackendEventListener implements EventListenerInterface
+{
+
+    /**
+     * Returns a list of events this object is implementing. When the class is registered
+     * in an event manager, each individual method will be associated with the respective event.
+     *
+     * @see EventListenerInterface::implementedEvents()
+     * @return array associative array or event key names pointing to the function
+     * that should be called in the object when the respective event is fired
+     */
+    public function implementedEvents()
+    {
+        return [
+            'Backend.Menu.get' => 'getBackendMenu'
+        ];
+    }
+
+    public function getBackendMenu(Event $event)
+    {
+        $event->subject()->addItem([
+            'title' => 'Settings',
+            'url' => ['plugin' => 'Banana', 'controller' => 'Settings', 'action' => 'index'],
+            'data-icon' => 'gears',
+        ]);
+    }
+}
