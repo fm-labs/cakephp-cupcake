ed1804eab7f5b8b01260afef940cc20fab42e5d0
fm-labs <flowmotionlabs@gmail.com>
2020-04-28 17:43:00 +0200

    added StatusBehavior and -Helper

---
+++ src/View/Helper/StatusHelper.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace Banana\View\Helper;
+
+use Banana\Lib\Status;
+use Cake\View\Helper;
+
+/**
+ * Class StatusHelper
+ *
+ * @package Banana\View\Helper
+ */
+class StatusHelper extends Helper
+{
+    /**
+     * Render status html
+     *
+     * @param $status
+     * @return string
+     * @todo Make use of UiHelper::label() or LabelHelper::status() from Bootstrap plugin
+     */
+    public function label($status)
+    {
+        if (is_object($status) && $status instanceof Status) {
+            return $status->toHtml();
+        }
+
+        return $status;
+    }
+}
