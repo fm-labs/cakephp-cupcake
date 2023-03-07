6017f42de4d894a3e6803ba9b9c602f93507c943
fm-labs <flowmotionlabs@gmail.com>
2023-03-07 23:26:48 +0100

    Added ThemeComponent

---
+++ src/Controller/Component/ThemeComponent.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+
+namespace Cupcake\Controller\Component;
+
+use Cake\Controller\Component;
+use Cake\Core\Configure;
+use Cake\Core\Plugin;
+
+/**
+ * Class FrontendComponent
+ *
+ * @package Content\Controller\Component
+ * @property \Cake\Controller\Component\FlashComponent $Flash
+ */
+class ThemeComponent extends Component
+{
+    public $components = ['Flash'];
+
+    /**
+     * @var array
+     */
+    protected $_defaultConfig = [
+        'viewClass' => null,
+        'theme' => null,
+        'layout' => null,
+    ];
+
+    /**
+     * @inheritDoc
+     */
+    public function initialize(array $config): void
+    {
+        $theme = $this->_config['theme'] ?: Configure::read('Theme.name');
+        $layout = $this->_config['layout'] ?: Configure::read('Theme.layout');
+        $viewClass = $this->_config['viewClass'] ?: null;
+
+        // check if theme plugin is loaded
+        if ($theme && !Plugin::isLoaded($theme)) {
+            $this->Flash->warning("Warning: Configured site theme '$theme' is not enabled.");
+            $theme = null;
+        }
+
+        $this->getController()->viewBuilder()->setClassName($viewClass);
+        $this->getController()->viewBuilder()->setLayout($layout);
+        $this->getController()->viewBuilder()->setTheme($theme);
+        $this->getController()->viewBuilder()
+            ->setVar('_frontend', compact('layout', 'theme', 'viewClass'));
+    }
+}
