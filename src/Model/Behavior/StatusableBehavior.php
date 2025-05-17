7c3387cc6b05e79a8e5d36617d6137c2596446fb
fm-labs <flowmotionlabs@gmail.com>
2025-05-17 10:41:52 +0200

    Added StatusableBehavior::findStatus finder

--- src/Model/Behavior/StatusableBehavior.php
+++ src/Model/Behavior/StatusableBehavior.php
@@ -3,12 +3,16 @@ declare(strict_types=1);
 
 namespace Cupcake\Model\Behavior;
 
-use ArrayObject;
+use ArrayAccess;
 use Cake\Collection\Iterator\MapReduce;
 use Cake\Datasource\EntityInterface;
+use Cake\Event\EventInterface;
 use Cake\ORM\Behavior;
 use Cake\ORM\Query;
+use Cake\ORM\Query\SelectQuery;
 use Cupcake\Lib\Status;
+use InvalidArgumentException;
+use function Cake\Core\deprecationWarning;
 
 /**
  * Statusable behavior
@@ -22,6 +26,9 @@ class StatusableBehavior extends Behavior
      */
     protected array $_defaultConfig = [
         'fields' => [],
+        'implementedFinders' => [
+            'status' => 'findStatus',
+        ],
         'implementedMethods' => [
             'getStatusCodes' => 'getStatusCodes',
             'getStatusList' => 'getStatusList',
@@ -32,7 +39,7 @@ class StatusableBehavior extends Behavior
     /**
      * @var array
      */
-    protected $_fieldConfig = [];
+    protected array $_fieldConfig = [];
 
     /**
      * @param array $config Behavior config
@@ -45,64 +52,40 @@ class StatusableBehavior extends Behavior
         //}
 
         if (!method_exists($this->_table, 'implementedStati')) {
-            throw new \InvalidArgumentException(sprintf("Table %s has no method 'implementedStati'", $this->_table->getAlias()));
+            throw new InvalidArgumentException(sprintf(
+                "Table %s has no method 'implementedStati'",
+                $this->_table->getAlias(),
+            ));
         }
 
         $this->_fieldConfig = call_user_func([$this->_table, 'implementedStati']);
     }
 
     /**
-     * @param $field
-     * @param array $stati
+     * @param string $field
+     * @param array $statusMap
      */
-    protected function _configureField($field, array $stati)
+    protected function _configureField(string $field, array $statusMap): void
     {
-        foreach ($stati as $status => $conf) {
+        foreach ($statusMap as $status => $conf) {
             $conf = array_merge(['label' => $status, 'class' => null], $conf);
             $this->_fieldConfig[$field][$status] = $conf;
         }
     }
 
     /**
+     * Attach status info to the query results
+     *
+     * Applies a MapReduce to the query, which resolves status info
+     * if a status field is present in the query results.
+     *
      * @param \Cake\ORM\Query $query
      * @param array $options
      * @return \Cake\ORM\Query
      */
-    public function findStatus(Query $query, array $options)
+    public function findStatus(Query $query, array $options): Query
     {
-        return $query;
-    }
-
-    /**
-     * 'beforeFind' callback
-     *
-     * Applies a MapReduce to the query, which resolves attachment info
-     * if an attachment field is present in the query results.
-     *
-     * @param \Cake\Event\Event $event
-     * @param \Cake\ORM\Query $query
-     * @param array $options
-     * @param $primary
-     */
-
-    /**
-     * 'beforeFind' callback
-     *
-     * Applies a MapReduce to the query, which resolves attachment info
-     * if an attachment field is present in the query results.
-     *
-     * @param \Cake\Event\Event $event
-     * @param \Cake\ORM\Query $query
-     * @param array $options
-     * @param $primary
-     */
-    public function beforeFind(\Cake\Event\EventInterface $event, Query $query, $options, $primary)
-    {
-        if (!isset($options['status']) || $options['status'] === false) {
-            return;
-        }
-
-        $mapper = function ($row, $key, MapReduce $mapReduce) {
+        $mapper = function ($row, $key, MapReduce $mapReduce): void {
 
             foreach (array_keys($this->_fieldConfig) as $fieldName) {
                 if (!isset($row[$fieldName])) {
@@ -122,19 +105,40 @@ class StatusableBehavior extends Behavior
             $mapReduce->emitIntermediate($row, $key);
         };
 
-        $reducer = function ($bucket, $name, MapReduce $mapReduce) {
+        $reducer = function ($bucket, $name, MapReduce $mapReduce): void {
             $mapReduce->emit($bucket[0], $name);
         };
 
         $query->mapReduce($mapper, $reducer);
+
+        return $query;
+    }
+
+
+    /**
+     * @param \Cake\Event\Event $event
+     * @param \Cake\ORM\Query\SelectQuery $query
+     * @param \ArrayAccess|array $options
+     * @param bool $primary
+     */
+    public function beforeFind(EventInterface $event, SelectQuery $query, ArrayAccess|array $options, bool $primary): void
+    {
+        if (isset($options['status']) && $options['status'] === true) {
+            deprecationWarning(
+                '4.0.1',
+                'The status query option is deprecated. Use the StatusableBehavior::findStatus() method instead.',
+            );
+            unset($options['status']);
+            $query->find('status'/*, (array)$options*/);
+        }
     }
 
     /**
      * @param \Cake\Event\Event $event
      * @param \Cake\Datasource\EntityInterface $entity
-     * @param \ArrayObject $options
+     * @param \ArrayAccess|array $options
      */
-    public function beforeSave(\Cake\Event\EventInterface $event, EntityInterface $entity, ArrayObject $options)
+    public function beforeSave(EventInterface $event, EntityInterface $entity, ArrayAccess|array $options): void
     {
         foreach (array_keys($this->_fieldConfig) as $fieldName) {
             if ($entity->get($fieldName) === null) {
@@ -152,7 +156,12 @@ class StatusableBehavior extends Behavior
         }
     }
 
-    public function getStatusCodes(string $field) {
+    /**
+     * @param string $field
+     * @return array
+     */
+    public function getStatusCodes(string $field): array
+    {
         $fieldStati = $this->_fieldConfig[$field] ?? null;
         if ($fieldStati === null) {
             return [];
@@ -161,20 +170,30 @@ class StatusableBehavior extends Behavior
         return array_keys($fieldStati);
     }
 
-    public function getStatusList(string $field) {
+    /**
+     * @param string $field
+     * @return array
+     */
+    public function getStatusList(string $field): array
+    {
         $fieldStati = $this->_fieldConfig[$field] ?? null;
         //var_dump($fieldStati);
         if ($fieldStati === null) {
             return [];
         }
 
-        return array_map(function($status) {
+        return array_map(function ($status) {
             /** @var \Cupcake\Lib\Status $status */
             return $status->getLabel();
         }, $fieldStati);
     }
 
-    public function getStatusSelectList(string $field) {
+    /**
+     * @param string $field
+     * @return array
+     */
+    public function getStatusSelectList(string $field): array
+    {
         $fieldStati = $this->_fieldConfig[$field] ?? null;
         //var_dump($fieldStati);
         if ($fieldStati === null) {
@@ -185,6 +204,7 @@ class StatusableBehavior extends Behavior
         foreach ($fieldStati as $status) {
             $selectList[$status->getStatus()] = $status->getLabel();
         }
+
         return $selectList;
     }
 }
